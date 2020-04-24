6ef1a8fe2f71841c61250d113e01d9874935dc8b
nick migel <nickmigel2@gmail.com>
2020-04-24 14:18:27 -0400

    finally finished 18 mins late

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -4,9 +4,6 @@ const server = require('../api/server')
 const db = require('../database/dbConfig')
 
 describe('server', () => {
-    beforeEach(async () => {
-        await db('users').truncate()
-    })
     describe('get /api/jokes', () => {
 
         it('fails', () => {
@@ -18,20 +15,41 @@ describe('server', () => {
     })
     describe('post /register', () => {
         it('fails when no info give', async () => {
+            await db('users').truncate()
             request(server).post('/api/auth/register')
                 .then(res => {
                     expect(res.status).toBe(500)
                 })
         })
         it('successfully adds a user to the db', async () => {
+
             await request(server).post('/api/auth/register')
-                .send({ username: 'testing', password: 'elbow' })
+                .send({ username: 'user', password: 'elbow' })
                 .then(res => {
                     expect(res.status).toBe(201)
                 })
 
             const inserted = await db('users')
             expect(inserted).toHaveLength(1)
+        })
+    })
+
+    describe('post /login', () => {
+
+        it('sends back 200 on log in', async () => {
+            await request(server).post('/api/auth/login')
+                .send({ username: 'user', password: 'elbow' })
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
+        })
+
+        it('sends back a token', async () => {
+            await request(server).post('/api/auth/login')
+                .send({ username: 'user', password: 'elbow' })
+                .then(res => {
+                    expect(res.body.payload).toBeTruthy()
+                })
         })
     })
 })
